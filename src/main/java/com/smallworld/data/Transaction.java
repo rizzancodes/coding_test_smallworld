8d0657013630372d2b4f164df1105a9e01a6c9fe
Rizzan <rimran@harriscomputer.com>
2023-12-29 22:21:43 +0500

    Added model class properties for Transaction class

--- src/main/java/com/smallworld/data/Transaction.java
+++ src/main/java/com/smallworld/data/Transaction.java
@@ -2,4 +2,18 @@ package com.smallworld.data;
 
 public class Transaction {
     // Represent your transaction data here.
+
+    private int mtn;
+    private double amount;
+
+
+    private String senderFullName;
+    private int senderAge;
+    private String beneficiaryFullName;
+    private int beneficiaryAge;
+    private int issueId;
+    private boolean issueSolved;
+    private String issueMessage;
+
+
 }
